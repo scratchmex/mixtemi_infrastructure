a50b70080b695656ef83e77ac33f9196f871f6bb
Ivan Gonzalez <scratchmex@gmail.com>
2022-05-07 17:38:19 -0500

    feat: add 3-empanada

--- infra-config/__main__.py
+++ infra-config/__main__.py
@@ -26,6 +26,7 @@ dns_man.add_record("CNAME", "dev", "app.@")
 # - empanada
 dns_man.add_record("CNAME", "api.empanada.app", "app.@")
 dns_man.add_record("CNAME", "api.2-empanada.app", "app.@")
+dns_man.add_record("CNAME", "api.3-empanada.app", "app.@")
 dns_man.add_record("CNAME", "api.empanada.dev", "dev.@")
 
 # - servers
